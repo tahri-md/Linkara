39de0796b12300ad6d0122165eac495e420388cd
tahri-md <tahrimedamine00@gmail.com>
2026-05-01 23:09:45 +0100

    chore(resolvers): wire pipeline resolvers into main schema
    - Import pipelineResolvers module
    - Register pipelineResolvers Query and Mutation into resolver map
    - Export PipelineRun type resolver for status formatting

--- src/graphql/resolvers/index.ts
+++ src/graphql/resolvers/index.ts
@@ -2,6 +2,7 @@ import { authResolvers } from './auth.js';
 import { organizationResolvers } from './organization.js';
 import { workflowResolvers } from './workflow.js';
 import { jobResolvers } from './job.js';
+import { pipelineResolvers } from './pipeline.js';
 
 export const resolvers = {
   Query: {
@@ -9,11 +10,14 @@ export const resolvers = {
     ...organizationResolvers.Query,
     ...workflowResolvers.Query,
     ...jobResolvers.Query,
+    ...pipelineResolvers.Query,
   },
   Mutation: {
     ...authResolvers.Mutation,
     ...organizationResolvers.Mutation,
     ...workflowResolvers.Mutation,
+    ...pipelineResolvers.Mutation,
   },
   Organization: organizationResolvers.Organization,
+  PipelineRun: pipelineResolvers.PipelineRun,
 };
