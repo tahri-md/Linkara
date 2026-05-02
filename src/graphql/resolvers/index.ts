03823bef7f175480eb4f6cb250784c037ef83dd3
tahri-md <tahrimedamine00@gmail.com>
2026-05-02 16:51:25 +0100

    chore(graphql): wire secrets schema into resolver map

--- src/graphql/resolvers/index.ts
+++ src/graphql/resolvers/index.ts
@@ -3,6 +3,7 @@ import { organizationResolvers } from './organization.js';
 import { workflowResolvers } from './workflow.js';
 import { jobResolvers } from './job.js';
 import { pipelineResolvers } from './pipeline.js';
+import { secretsResolvers } from './secrets.js';
 
 export const resolvers = {
   Query: {
@@ -11,12 +12,14 @@ export const resolvers = {
     ...workflowResolvers.Query,
     ...jobResolvers.Query,
     ...pipelineResolvers.Query,
+    ...secretsResolvers.Query,
   },
   Mutation: {
     ...authResolvers.Mutation,
     ...organizationResolvers.Mutation,
     ...workflowResolvers.Mutation,
     ...pipelineResolvers.Mutation,
+    ...secretsResolvers.Mutation,
   },
   Organization: organizationResolvers.Organization,
   PipelineRun: pipelineResolvers.PipelineRun,
--- src/graphql/schema.ts
+++ src/graphql/schema.ts
@@ -8,12 +8,14 @@ const userSchema = readFileSync(resolve(__dirname, './types/user.graphql'), 'utf
 const organizationSchema = readFileSync(resolve(__dirname, './types/organization.graphql'), 'utf-8');
 const workflowSchema = readFileSync(resolve(__dirname, './types/workflow.graphql'), 'utf-8');
 const jobSchema = readFileSync(resolve(__dirname, './types/job.graphql'), 'utf-8');
+const secretsSchema = readFileSync(resolve(__dirname, './types/secrets.graphql'), 'utf-8');
 
 export const typeDefs = `
   ${userSchema}
   ${organizationSchema}
   ${workflowSchema}
   ${jobSchema}
+  ${secretsSchema}
 `;
 
 export interface Context {
